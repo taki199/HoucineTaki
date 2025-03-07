868e4302b291a090bb25d0909a4239d1867880f5
HOUCINE TAKI <christotaki22@gmail.com>
2025-03-07 13:49:47 +0000

    api key

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,13 @@ import "./index.css";
 import { PostHogProvider } from "posthog-js/react";
 
 const options = {
-  api_host: process.env.REACT_APP_PUBLIC_POSTHOG_HOST,
+  api_host: import.meta.env.REACT_APP_PUBLIC_POSTHOG_HOST,
 };
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <PostHogProvider
-      apiKey={process.env.REACT_APP_PUBLIC_POSTHOG_KEY}
+      apiKey={import.meta.env.REACT_APP_PUBLIC_POSTHOG_KEY}
       options={options}
     >
       <App />
