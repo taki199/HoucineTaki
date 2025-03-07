887ac670c2f4d54b56eca7b8b645ffd274b73faf
HOUCINE TAKI <christotaki22@gmail.com>
2025-03-07 14:19:37 +0000

    analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 
+import posthog from "posthog-js";
 import { PostHogProvider } from "posthog-js/react";
 
-const options = {
+posthog.init(import.meta.env.REACT_APP_PUBLIC_POSTHOG_KEY, {
   api_host: import.meta.env.REACT_APP_PUBLIC_POSTHOG_HOST,
-};
-
+});
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <PostHogProvider
-      apiKey={import.meta.env.REACT_APP_PUBLIC_POSTHOG_KEY}
-      options={options}
-    >
+    <PostHogProvider client={posthog}>
       <App />
     </PostHogProvider>
   </StrictMode>
